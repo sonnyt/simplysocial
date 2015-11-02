18c4f905aca3fb4281eddafa0dfb5579a250c25f
Sonny T <mail@sonnyt.com>
2015-11-02 10:46:46 -0500

    Updated footer links

--- app/core/components/footer.js
+++ app/core/components/footer.js
@@ -7,10 +7,10 @@ class Footer extends Component {
     render() {
         return (
             <footer className="footer">
-                <a href>About Us</a>
-                <a href>Support</a>
-                <a href>Privacy</a>
-                <a href>Terms</a>
+                <a href="#">About Us</a>
+                <a href="#">Support</a>
+                <a href="#">Privacy</a>
+                <a href="#">Terms</a>
                 <span>&copy; 2014 simplysocial</span>
             </footer>
         )
